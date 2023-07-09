020a6051afc8c714b15eec6634cf1a5e714ab03a
Michael B. Espeña <michael.espena.public@gmail.com>
2023-07-09 19:40:58 +0800

    add solution for ch05/ex3.hs

---
+++ exercises/ch05/ex3.hs
@@ -0,0 +1,14 @@
+module Ex3 where
+
+-- Using a list comprehension and the function `grid` above, define a function
+-- `square :: Int -> [(Int, Int)]` that returns a coordinate square of size n,
+-- excluding the diagonal from (0, 0) to (n, n). For example:
+--
+-- > square 2
+-- [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
+
+grid :: Int -> Int -> [(Int, Int)]
+grid x y = [(xi, yi) | xi <- [0 .. x], yi <- [0 .. y]]
+
+square :: Int -> [(Int, Int)]
+square n = [(x, y) | (x, y) <- grid n n, x /= y]
