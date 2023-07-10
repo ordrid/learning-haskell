ca5f6d97f2ef6b3a7f84de1054ac071c81ebb8d0
Michael B. Espeña <michael.espena.public@gmail.com>
2023-07-10 09:06:44 +0800

    rename variables

--- exercises/ch05/ex3.hs
+++ exercises/ch05/ex3.hs
@@ -8,7 +8,7 @@ module Ex3 where
 -- [(0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1)]
 
 grid :: Int -> Int -> [(Int, Int)]
-grid x y = [(xi, yi) | xi <- [0 .. x], yi <- [0 .. y]]
+grid x y = [(x', y') | x' <- [0 .. x], y' <- [0 .. y]]
 
 square :: Int -> [(Int, Int)]
 square n = [(x, y) | (x, y) <- grid n n, x /= y]
